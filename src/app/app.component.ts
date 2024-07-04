ca4fcad810ed9a925e879743e229b7397d4b32d7
Andrew Cross <andrew-stuart-cross@outlook.com>
2024-07-04 08:57:44 +0100

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LineComponent } from './line/line.component';
+import { BarComponent } from './bar/bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LineComponent],
+  imports: [CommonModule, RouterOutlet, LineComponent, BarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/bar/bar.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { ChartConfiguration } from 'chart.js';
+import { IChartData } from '../i-chart-data.dto';
+import { BaseChartDirective } from 'ng2-charts';
+
+@Component({
+  selector: 'app-bar',
+  standalone: true,
+  imports: [BaseChartDirective],
+  templateUrl: './bar.component.html',
+  styleUrl: './bar.component.css'
+})
+export class BarComponent {
+  public data: IChartData;
+
+  constructor() {
+    this.data = {
+      chartData: [
+        {
+          data: [39, 39, 29, 22, 20, 20, 14, 2, 2],
+          label: 'caps'
+        },
+        {
+          data: [149, 117, 103, 87, 84, 82, 58, 11, 9],
+          label: 'wickets'
+        }
+      ],
+      chartLabels: ["v India", "v Australia", "v South Africa", "v West Indies", "v New Zealand", "v Pakistan", "v Sri Lanka", "v Zimbabwe", "v Bangladesh"]
+    }
+  }
+
+
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: true,
+    indexAxis: 'y',
+    plugins: {
+      title: {
+          display: true,
+          text: 'Custom Chart Title'
+      }
+  }
+  };
+}
