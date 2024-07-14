cb499bace6eef98ea5f479409bb4a36881bcdb4a
Andrew Cross <andrew-stuart-cross@outlook.com>
2024-07-14 16:10:54 +0100

    Update strike rate with new data; new bar/line combo chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { LineComponent } from './line/line.component';
 import { BarComponent } from './bar/bar.component';
 import { EconomyRateComponent } from './economy-rate/economy-rate.component';
 import { StrikeRateComponent } from './strike-rate/strike-rate.component';
+import { WktsMatchComponent } from "./wkts-match/wkts-match.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LineComponent, BarComponent, EconomyRateComponent, StrikeRateComponent],
+  imports: [CommonModule, RouterOutlet, LineComponent, BarComponent, EconomyRateComponent, StrikeRateComponent, WktsMatchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
