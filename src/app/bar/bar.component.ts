d82f428f85a918eb2de3b46dc47719f9a86ac6c8
Andrew Cross <andrew-stuart-cross@outlook.com>
2024-07-15 11:24:24 +0100

    update bar chart data; add toggle switch to hide/show series

--- src/app/bar/bar.component.ts
+++ src/app/bar/bar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal, Signal, ViewChild } from '@angular/core';
 import { ChartConfiguration } from 'chart.js';
 import { IChartData } from '../i-chart-data.dto';
 import { BaseChartDirective } from 'ng2-charts';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-bar',
@@ -11,21 +12,44 @@ import { BaseChartDirective } from 'ng2-charts';
   styleUrl: './bar.component.css'
 })
 export class BarComponent {
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+
+  public chartPlugins = [ChartDataLabels];
   public data: IChartData;
+
+  public btnText = signal('show 5w count');
+
 
   constructor() {
     this.data = {
       chartData: [
         {
-          data: [39, 39, 29, 22, 20, 20, 14, 2, 2],
-          label: 'caps'
+          data: [149, 117, 103, 91, 84, 82, 58, 11, 9],
+          label: 'matches'
         },
         {
-          data: [149, 117, 103, 87, 84, 82, 58, 11, 9],
+          data: [39, 39, 29, 23, 20, 20, 14, 2, 2],
           label: 'wickets'
+        },
+        {
+          data: [6, 5, 4, 5, 3, 3, 5, 1, 0],
+          label: '5 wickets',
+          hidden: true
         }
       ],
       chartLabels: ["v India", "v Australia", "v South Africa", "v West Indies", "v New Zealand", "v Pakistan", "v Sri Lanka", "v Zimbabwe", "v Bangladesh"]
+    }
+  }
+
+  public toggle5w(): void {
+    if (this.data?.chartData[2].hidden){
+      this.data.chartData[2].hidden=false;
+      this.chart?.update();
+      this.btnText.set('hide 5w count');
+    } else {
+      this.data.chartData[2].hidden=true;
+      this.chart?.update();
+      this.btnText.set('show 5w count');
     }
   }
 
@@ -34,13 +58,17 @@ export class BarComponent {
     responsive: true,
     indexAxis: 'y',
     plugins: {
+      datalabels: {
+        align: 'end',
+        anchor: 'end'
+      },
       title: {
         display: true,
-        text: 'JM Anderson'
+        text: 'Matches and wickets taken by opponent'
       },
       subtitle: {
         display: true,
-        text: 'Matches and wickets taken by opponent',
+        text: 'JM Anderson',
         // color: 'blue',
         // font: {
         //   size: 12,
