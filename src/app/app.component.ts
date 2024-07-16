9cda395e74b663279f5ac474e8fa43217034d92b
Andrew Cross <andrew-stuart-cross@outlook.com>
2024-07-16 17:04:15 +0100

    separate into specialist components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,14 @@ import { EconomyRateComponent } from './economy-rate/economy-rate.component';
 import { StrikeRateComponent } from './strike-rate/strike-rate.component';
 import { WktsMatchComponent } from "./wkts-match/wkts-match.component";
 import { MultiAxisLineComponent } from './multi-axis-line/multi-axis-line.component';
+import { AgeAnalysisLineComponent } from './age-analysis-line/age-analysis-line.component';
+import { FormatAnalysisLineComponent } from './format-analysis-line/format-analysis-line.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LineComponent, BarComponent, EconomyRateComponent, StrikeRateComponent, WktsMatchComponent, MultiAxisLineComponent],
+  imports: [CommonModule, RouterOutlet, LineComponent, BarComponent, EconomyRateComponent,
+    StrikeRateComponent, WktsMatchComponent, MultiAxisLineComponent, AgeAnalysisLineComponent, FormatAnalysisLineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
